b455d491a8a4a69f20c730cb539015dfaa76b76a
chenaoh <jfarova112333@gmail.com>
2022-11-20 20:21:51 -0500

    Programa que lea un número entero por teclado y que calcule su factorial

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num,fac=1;
+        Console.WriteLine("ingrese numero");
+        num=int.Parse(Console.ReadLine());
+        while(num !=0){
+            fac = fac * num;
+            num=num-1;
+        }
+        Console.WriteLine("el factorial es: " + fac);
+        Console.ReadKey(true);
+    }
+}
